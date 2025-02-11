f0c5c690edc195fe71cdddd99872ab61a50cf6db
Dennis Guajardo <dguaja3@wgu.edu>
2025-02-11 11:06:31 -0800

    finalizing the pages second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {PetParadigmComponent} from './pet-paradigm/pet-paradigm.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {RouterOutlet} from '@angular/router';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, FooterComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
